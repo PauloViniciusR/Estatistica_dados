aeb63605aa308570b5196b9a0a49dcad26b9f154
Paulo Vinícius <vinicrf81@gmail.com>
2026-03-14 18:06:21 -0300

    estatistica

--- Estatistica_Descritiva/auxiliares.py
+++ Estatistica_Descritiva/auxiliares.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def tabela_distribuicao_frequencias(dataframe, coluna):
+def tabela_distribuicao_frequencias(dataframe, coluna, coluna_frequencia=False):
     """Cria uma tabela de distribuição de frequências para uma coluna de um dataframe.
     Espera uma coluna categórica.
 
@@ -11,6 +11,8 @@ def tabela_distribuicao_frequencias(dataframe, coluna):
         Dataframe com os dados.
     coluna : str
         Nome da coluna categórica.
+    coluna_frequencia : bool
+        Informa se a coluna passada ja é com os valores de frequencia ou não. Padão: False
 
     Returns
     -------
@@ -19,9 +21,14 @@ def tabela_distribuicao_frequencias(dataframe, coluna):
     """ 
     
     df_estatistica = pd.DataFrame()
-    
-    df_estatistica["frequencia"] = dataframe[coluna].value_counts().sort_index()
-    df_estatistica["frequencia_relativa"] = dataframe[coluna].value_counts(normalize=True).sort_index()
+
+    if coluna_frequencia:
+        df_estatistica["frequencia"] = dataframe[coluna]
+        df_estatistica["frequencia_relativa"] = df_estatistica["frequencia"] / df_estatistica["frequencia"].sum()
+    else:
+        df_estatistica["frequencia"] = dataframe[coluna].value_counts().sort_index()
+        df_estatistica["frequencia_relativa"] = dataframe[coluna].value_counts(normalize=True).sort_index()
+        
     df_estatistica["frequencia_acumulada"] = df_estatistica["frequencia"].cumsum()
     df_estatistica["frequencia_relativa_acumulada"] = df_estatistica["frequencia_relativa"].cumsum()
     
