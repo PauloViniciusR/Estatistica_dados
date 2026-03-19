c4d4bbfbde6fc902a803265a841a3490fc62d283
Paulo Vinícius <vinicrf81@gmail.com>
2026-03-18 22:24:40 -0300

    estatistica

--- Estatistica_Descritiva/auxiliares.py
+++ Estatistica_Descritiva/auxiliares.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 
 def tabela_distribuicao_frequencias(dataframe, coluna, coluna_frequencia=False):
@@ -33,3 +35,47 @@ def tabela_distribuicao_frequencias(dataframe, coluna, coluna_frequencia=False):
     df_estatistica["frequencia_relativa_acumulada"] = df_estatistica["frequencia_relativa"].cumsum()
     
     return df_estatistica
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+def composicao_boxplot(dataframe, coluna, intervalos="auto"):
+    fig, (ax1, ax2) = plt.subplots(
+        nrows=2, 
+        ncols=1, 
+        sharex=True,
+        gridspec_kw={
+            "height_ratios": (0.15, 0.85),
+            "hspace": 0.02
+        }
+    )
+
+    sns.boxplot(
+        data=dataframe, 
+        x=coluna, 
+        showmeans=True, 
+        meanline=True,
+        meanprops={"color": "C1", "linewidth": 1.5, "linestyle": "--"},
+        medianprops={"color": "C2", "linewidth": 1.5, "linestyle": "--"},
+        ax=ax1,
+    )
+
+    sns.histplot(
+        data=dataframe, 
+        x=coluna, 
+        kde=True, 
+        bins=intervalos, 
+        ax=ax2
+    )
+
+    for ax in (ax1, ax2):
+        ax.grid(True, linestyle="--", color="gray", alpha=0.5)
+        ax.set_axisbelow(True)
+
+    ax2.axvline(dataframe[coluna].mean(), color='C1', linestyle="--", label=("Média"))
+    ax2.axvline(dataframe[coluna].median(), color='C2', linestyle="--", label=("Mediana"))
+    ax2.axvline(dataframe[coluna].mode()[0], color='C3', linestyle="--", label=("Moda"))
+
+
+    ax2.legend()
+
+    plt.show()
