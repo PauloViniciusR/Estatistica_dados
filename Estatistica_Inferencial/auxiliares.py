8e4cb25031e5f1ec2fbde1552310d966466b63ed
Paulo Vinícius <vinicrf81@gmail.com>
2026-04-09 15:53:05 -0300

    EStatistica inferencial

--- Estatistica_Inferencial/auxiliares.py
+++ Estatistica_Inferencial/auxiliares.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-from scipy.stats import shapiro, levene, ttest_ind, ttest_rel
+from scipy.stats import f_oneway, levene, shapiro, ttest_ind, ttest_rel
 
 
 def tabela_distribuicao_frequencias(dataframe, coluna, coluna_frequencia=False):
@@ -15,7 +15,7 @@ def tabela_distribuicao_frequencias(dataframe, coluna, coluna_frequencia=False):
     coluna : str
         Nome da coluna categórica.
     coluna_frequencia : bool
-        Informa se a coluna passada ja é com os valores de frequencia ou não. Padão: False
+        Informa se a coluna passada já é com os valores de frequência ou não. Padrão: False
 
     Returns
     -------
@@ -37,119 +37,129 @@ def tabela_distribuicao_frequencias(dataframe, coluna, coluna_frequencia=False):
     
     return df_estatistica
 
-#-----------------------------------------------------------------------------------------------------------------------
 
-def composicao_boxplot(dataframe, coluna, intervalos="auto"):
+def composicao_histograma_boxplot(dataframe, coluna, intervalos="auto"):
     fig, (ax1, ax2) = plt.subplots(
-        nrows=2, 
-        ncols=1, 
+        nrows=2,
+        ncols=1,
         sharex=True,
         gridspec_kw={
             "height_ratios": (0.15, 0.85),
             "hspace": 0.02
         }
     )
-
+    
     sns.boxplot(
-        data=dataframe, 
-        x=coluna, 
-        showmeans=True, 
+        data=dataframe,
+        x=coluna,
+        showmeans=True,
         meanline=True,
         meanprops={"color": "C1", "linewidth": 1.5, "linestyle": "--"},
         medianprops={"color": "C2", "linewidth": 1.5, "linestyle": "--"},
         ax=ax1,
     )
-
-    sns.histplot(
-        data=dataframe, 
-        x=coluna, 
-        kde=True, 
-        bins=intervalos, 
-        ax=ax2
-    )
-
+    
+    sns.histplot(data=dataframe, x=coluna, kde=True, bins=intervalos, ax=ax2)
+    
     for ax in (ax1, ax2):
         ax.grid(True, linestyle="--", color="gray", alpha=0.5)
         ax.set_axisbelow(True)
-
-    ax2.axvline(dataframe[coluna].mean(), color='C1', linestyle="--", label=("Média"))
-    ax2.axvline(dataframe[coluna].median(), color='C2', linestyle="--", label=("Mediana"))
-    ax2.axvline(dataframe[coluna].mode()[0], color='C3', linestyle="--", label=("Moda"))
-
-
+    
+    ax2.axvline(dataframe[coluna].mean(), color="C1", linestyle="--", label="Média")
+    ax2.axvline(dataframe[coluna].median(), color="C2", linestyle="--", label="Mediana")
+    ax2.axvline(dataframe[coluna].mode()[0], color="C3", linestyle="--", label="Moda")
+    
     ax2.legend()
-
+    
     plt.show()
 
 def analise_shapiro(dataframe, alfa=0.05):
     print("Teste de Shapiro-Wilk")
     for coluna in dataframe.columns:
-        estatistica, pvalue = shapiro(dataframe[coluna], nan_policy="omit")
-        print(f"{estatistica=:.3f}")
-        if pvalue > alfa:
-            print(f"{coluna} segue uma distribuição normal (valor p: {pvalue=:.3f})")
+        estatistica_sw, valor_p_sw = shapiro(dataframe[coluna], nan_policy="omit")
+        print(f"{estatistica_sw=:.3f}")
+        if valor_p_sw > alfa:
+            print(f"{coluna} segue uma distribuição normal (valor p: {valor_p_sw:.3f})")
         else:
-            print(f"{coluna} não segue uma distribuição normal (valor p: {pvalue=:.3f})")
-
-def analise_levene(dataframe, alfa=0.05, center="mean"):
-    print("Teste de levene")
-
-    estatistica_levene, pvalue = levene(
+            print(f"{coluna} não segue uma distribuição normal (valor p: {valor_p_sw:.3f})")
+        
+        
+def analise_levene(dataframe, alfa=0.05, centro="mean"):
+    print("Teste de Levene")
+    
+    estatistica_levene, valor_p_levene = levene(
         *[dataframe[coluna] for coluna in dataframe.columns], 
-        center=center, 
-        nan_policy="omit")
-
+        center=centro, 
+        nan_policy="omit"
+    )
+        
     print(f"{estatistica_levene=:.3f}")
-    if pvalue > alfa:
-        print(f"Variancias iguais (valor p: {pvalue=:.3f})")
+    if valor_p_levene > alfa:
+        print(f"Variâncias iguais (valor p: {valor_p_levene:.3f})")
     else:
-        print(f"Ao menos uma variancia diferente (valor p: {pvalue=:.3f})")
+        print(f"Ao menos uma variância é diferente (valor p: {valor_p_levene:.3f})")
 
-def analises_shapiro_levene(dataframe, alfa=0.05, center="mean"):
+def analises_shapiro_levene(dataframe, alfa=0.05, centro="mean"):
     analise_shapiro(dataframe, alfa)
 
     print()
 
-    analise_levene(dataframe, alfa, center)
+    analise_levene(dataframe, alfa, centro)
+
 
 def analise_ttest_ind(
     dataframe,
     alfa=0.05,
-    variancia_igual=True,
-    alternative="two-sided",
+    variancias_iguais=True,
+    alternativa="two-sided",
 ):
-    
-    estatistica_ttest, pvalue_ttest = ttest_ind(
-        *[dataframe[coluna] for coluna in dataframe.columns],
-        equal_var=variancia_igual,
-        alternative=alternative,
+    print("Teste t de Student")
+    estatistica_ttest, valor_p_ttest = ttest_ind(
+        *[dataframe[coluna] for coluna in dataframe.columns], 
+        equal_var=variancias_iguais,
+        alternative=alternativa,
         nan_policy="omit"
     )
-
+        
     print(f"{estatistica_ttest=:.3f}")
-
-    if pvalue_ttest > alfa:
-        print(f"Não rejeita a hipótese nula (valor p: {pvalue_ttest:.3f})")
+    if valor_p_ttest > alfa:
+        print(f"Não rejeita a hipótese nula (valor p: {valor_p_ttest:.3f})")
     else:
-        print(f"Rejeita a hipótese nula (valor p: {pvalue_ttest:.3f})")
+        print(f"Rejeita a hipótese nula (valor p: {valor_p_ttest:.3f})")
+
 
 def analise_ttest_rel(
     dataframe,
     alfa=0.05,
-    alternative="two-sided",
+    alternativa="two-sided",
 ):
-    
-    estatistica_ttest, pvalue_ttest = ttest_rel(
-        *[dataframe[coluna] for coluna in dataframe.columns],
-        alternative=alternative,
+    print("Teste t de Student")
+    estatistica_ttest, valor_p_ttest = ttest_rel(
+        *[dataframe[coluna] for coluna in dataframe.columns], 
+        alternative=alternativa,
         nan_policy="omit"
     )
-
+        
     print(f"{estatistica_ttest=:.3f}")
-
-    if pvalue_ttest > alfa:
-        print(f"Não rejeita a hipótese nula (valor p: {pvalue_ttest:.3f})")
+    if valor_p_ttest > alfa:
+        print(f"Não rejeita a hipótese nula (valor p: {valor_p_ttest:.3f})")
     else:
-        print(f"Rejeita a hipótese nula (valor p: {pvalue_ttest:.3f})")
+        print(f"Rejeita a hipótese nula (valor p: {valor_p_ttest:.3f})")
 
 
+def analise_anova_one_way(
+    dataframe,
+    alfa=0.05,
+):
+    
+    print("Teste ANOVA one way")
+    estatistica_f, valor_p_f = f_oneway(
+        *[dataframe[coluna] for coluna in dataframe.columns], 
+        nan_policy="omit"
+    )
+        
+    print(f"{estatistica_f=:.3f}")
+    if valor_p_f > alfa:
+        print(f"Não rejeita a hipótese nula (valor p: {valor_p_f:.3f})")
+    else:
+        print(f"Rejeita a hipótese nula (valor p: {valor_p_f:.3f})")
